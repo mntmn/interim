772df18767ac72bd362f2ab22b8642feeeb6dc7f
mntmn <lukas@mnt.mn>
2015-07-17 18:59:35 +0000

    working sdl/raspian jit test

--- bjos/sledge/rpi_sdl_test.c
+++ bjos/sledge/rpi_sdl_test.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <sys/mman.h>
 
+#include "SDL/SDL.h"
+
 #define CODESZ 1024*4
 
 typedef uint32_t (*funcptr)();
@@ -15,23 +17,36 @@ void hello() {
   printf("hello!\n");
 }
 
+SDL_Surface* screen;
+
+void setup_sdl() {
+  SDL_Init(SDL_INIT_VIDEO);
+  screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
+}
+
 int main() {
+  setup_sdl();
+
   code = mmap(0, CODESZ, PROT_READ | PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, 0, 0);
   memset(code, 0, CODESZ);
   cpool_idx = 128; // 128 ops gap
 
-  uint32_t* fb = malloc(1024);
+  uint32_t* fb = (uint32_t*)screen->pixels;
 
   jit_movi(1,0);
   jit_movi(5,0);
-  jit_movi(2,0xffffff);
+  jit_movi(4,4);
+  jit_movi(6,1);
+  jit_movi(7,640*480);
+  jit_movi(2,0xff00ff);
   jit_lea(3,fb);
   jit_label("loop");
   
   jit_strw(2);
   jit_addr(3,4);
-  jit_addr(5,1);
-  jit_cmpi(5,0xff);
+  jit_addr(5,6);
+  jit_addr(2,6);
+  jit_cmpr(5,7);
   
   jit_jne("loop");
   jit_ret();
@@ -47,6 +62,12 @@ int main() {
   funcptr fn = (funcptr)code;
   uint32_t res = fn();
   printf("asm result: %lx\n",res);
+  SDL_UpdateRect(screen, 0, 0, 640, 480);
+
+  printf("done\n");
+ 
+  char* foo=malloc(64); 
+  gets(foo);
 
   //free(code);
   //munmap(code);
