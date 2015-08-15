6dbdd96ff1cacc3ea3f8b57359f8a3e77f777e43
mntmn <lukas@mnt.mn>
2015-08-15 13:40:30 +0200

    debug

--- sledge/compiler_arm_hosted.c
+++ sledge/compiler_arm_hosted.c
@@ -1,11 +1,11 @@
-#define CODESZ 4096
+#define CODESZ 8192
 #include <unistd.h>
 #include <fcntl.h>
 
 int compile_for_platform(Cell* expr, Cell** res) {
   code = mmap(0, CODESZ, PROT_READ | PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, 0, 0);
   memset(code, 0, CODESZ);
-  jit_init(512);
+  jit_init(0x400);
   //cpool_idx = CODESZ/2; // 128 ops gap
   //code_idx = 0;
 
