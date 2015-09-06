7781c938c99132bfd7be6ad25d373745eccaa710
mntmn <lukas@mnt.mn>
2015-09-06 22:37:21 +0200

    cleanup for release 0.1.0

---
+++ rpi2-release.sh
@@ -0,0 +1,7 @@
+cp ./build/kernel7.img ./release-rpi2/
+cp ./sledge/unifont.bin ./release-rpi2/
+cp ./sledge/os/shell.l ./release-rpi2/
+cp ./sledge/os/editor.l ./release-rpi2/
+cp ./sledge/os/paint.l ./release-rpi2/
+rm docs/interim-0.1.0-rpi2.tgz
+tar cfz docs/interim-0.1.0-rpi2.tgz ./release-rpi2
