498f4f851011402cdd9aa4cb8fffb2fe4a791dec
mntmn <lukas@mnt.mn>
2015-09-11 18:52:14 +0200

    binary cleanup, grub config + iso deploy fix

--- deploy_386.sh
+++ deploy_386.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p build/386iso/isodir
 mkdir -p build/386iso/boot
-cp build/interim.386 build/386iso/boot/interim.386
+cp build/interim.bin build/386iso/boot/interim.386
 mkdir -p build/386iso/boot/grub
 cp devices/grub.cfg build/386iso/boot/grub/grub.cfg
 grub-mkrescue -o interim386.iso build/386iso
