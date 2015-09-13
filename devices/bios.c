2afab5386a4624711fa7182133a7273fa61deb2e
mntmn <lukas@mnt.mn>
2015-09-13 16:52:02 +0200

    cleanup; iso qemu test

--- devices/bios.c
+++ devices/bios.c
@@ -6,16 +6,6 @@
 
 static void* heap_end;
  
-/* Check if the compiler thinks if we are targeting the wrong operating system. */
-//#if defined(__linux__)
-//#error "You are not using a cross-compiler, you will most certainly run into trouble"
-//#endif
- 
-/* This tutorial will only work for the 32-bit ix86 targets. */
-#if !defined(__i386__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
-#endif
- 
 /* Hardware text mode color constants. */
 enum vga_color {
 	COLOR_BLACK = 0,
---
+++ runiso-386.sh
@@ -0,0 +1,1 @@
+qemu-system-i386 -m 512M interim386.iso
