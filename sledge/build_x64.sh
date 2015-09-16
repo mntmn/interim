569dabb1a9ce3aebea3bbfddf7c645e2e6f47c0d
Jay McCarthy <jay.mccarthy@gmail.com>
2015-09-16 10:31:47 -0400

    Adding CFLAGS for Darwin/MacPorts

--- sledge/build_x64.sh
+++ sledge/build_x64.sh
@@ -1,2 +1,8 @@
-gcc -g -o sledge --std=gnu99 -I. sledge.c reader.c writer.c alloc.c strmap.c stream.c ../devices/sdl2.c ../devices/posixfs.c -lm -lSDL2 -DCPU_X64 -DDEV_SDL -DDEV_POSIXFS
+#!/bin/sh
+
+if [ `uname` = "Darwin" ] ; then
+    CFLAGS="${CFLAGS} -I/opt/local/include -L/opt/local/lib"
+fi
+
+cc -g -o sledge --std=gnu99 -I. ${CFLAGS} sledge.c reader.c writer.c alloc.c strmap.c stream.c ../devices/sdl2.c ../devices/posixfs.c -lm -lSDL2 -DCPU_X64 -DDEV_SDL -DDEV_POSIXFS
 
