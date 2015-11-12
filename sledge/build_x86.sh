567d5c69e483f3e3e99ca03e50cda851c5cb6aa2
mntmn <lukas@mnt.mn>
2015-11-12 19:33:51 +0100

    sdl back in x86; simplify repl/editor spawn funcs

--- sledge/build_x86.sh
+++ sledge/build_x86.sh
@@ -1,4 +1,3 @@
 
-gcc -m32 -g -o sledge --std=gnu99 -I. sledge.c reader.c writer.c alloc.c strmap.c stream.c ../devices/posixfs.c -lm -DCPU_X86 -DDEV_POSIXFS
+gcc -m32 -g -o sledge --std=gnu99 -I. sledge.c reader.c writer.c alloc.c strmap.c stream.c ../devices/posixfs.c ../devices/sdl2.c -lSDL2 -DDEV_SDL -lm -DCPU_X86 -DDEV_POSIXFS
 
-#  ../devices/sdl2.c -lSDL2 -DDEV_SDL
